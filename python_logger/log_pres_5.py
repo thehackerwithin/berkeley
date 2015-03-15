ac1481725e2ad98094e5755e77fbc332aec3ffd8
ddwooten <ddwooten@rocketmail.com>
2015-03-15 15:28:37 -0700

    Made it so log_pres_5 doesn't reprint to screen logging outputs, this also demonstrates duality behavior of a logger

--- python_logger/log_pres_5.py
+++ python_logger/log_pres_5.py
@@ -10,25 +10,25 @@ import logging as log
 log.basicConfig( format = '[%(name)s - %(levelname)8s] %(message)s' \
 	, level = 10 )
 # Some sample messages for the root logger
-log.debug( "This is the debug level reporting in" )
-log.info( "This is the info level reporting in " )
-log.warning( "This is the warning level reporting in" )
-log.error( "This is the error level reporting in" )
-log.critical( "This is the critical level reporting in" )
+log.debug( "This is the debug level on root reporting in" )
+log.info( "This is the info level on root reporting in " )
+log.warning( "This is the warning level on root reporting in" )
+log.error( "This is the error level on root reporting in" )
+log.critical( "This is the critical level on root reporting in" )
 
 # Create a new log class instance. Why, it can have
 # different behaviors than the root logger
-new_instance_log = log.getLogger( 'seperate_instance' )
+new_instance_log = log.getLogger( 'new' )
 
 # Set the new instance's log level as well as format with .basicConfig
 new_instance_log.setLevel( 10 )
 
 # Some sample messages. Can you guess which will print?
-new_instance_log.debug( "This is the debug level reporting in" )
-new_instance_log.info( "This is the info level reporting in " )
-new_instance_log.warning( "This is the warning level reporting in" )
-new_instance_log.error( "This is the error level reporting in" )
-new_instance_log.critical( "This is the critical level reporting in" )
+new_instance_log.debug( "This is the debug level on new reporting in" )
+new_instance_log.info( "This is the info level on new reporting in " )
+new_instance_log.warning( "This is the warning on new level reporting in" )
+new_instance_log.error( "This is the error level on new reporting in" )
+new_instance_log.critical( "This is the critical level on new reporting in" )
 
 # Now we create a "handler" for the new instance, this will
 # modifty its behavior, most importantly by redirecting the
@@ -44,6 +44,10 @@ not_serious.setFormatter( ns_formatter )
 # This is the real power of handlers
 # Add the handler to the new_instance_log
 new_instance_log.addHandler(not_serious)
+
+# Set the base level for this instance much higher so we don't
+# reprint to screen
+new_instance_log.setLevel(80)
 
 # Some sample messages. Can you guess which will print, and to where?
 new_instance_log.debug( "This is the debug level reporting in" )
